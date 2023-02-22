a0bb100f253f9b7ce57860ee1e14819e1c3b5fac
Nicolas German Olmos <desa.nolmos@gmail.com>
2023-02-22 12:15:12 -0300

    feature: success screen styling

--- src/components/SuccessScreen/SuccessScreen.js
+++ src/components/SuccessScreen/SuccessScreen.js
@@ -1,29 +1,36 @@
-import styles from './SuccessScreen.module.css';
+import './SuccessScreen.css';
 
 const SuccessScreen = (props) => {
 
   return (
-    <section>
-      <table>
-        <thead>
-          <tr>
-            <th>
-              Key
-            </th>
-            <th>
-              Value
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            Object.keys(props.dniData).map(key => <tr>
-              <td>{key}</td>
-              <td>{props.dniData[key].toString()}</td>
-            </tr>)
-          }
-        </tbody>
-      </table>
+    <section className='success-screen-page'>
+      <div className='success-screen-table-container'>
+        <header>
+          <h2>
+            This is your personal information
+          </h2>
+        </header>
+        <table className='success-screen-table'>
+          <thead>
+            <tr>
+              <th>
+                Key
+              </th>
+              <th>
+                Value
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              Object.keys(props.dniData).map(key => <tr>
+                <td>{key}</td>
+                <td>{props.dniData[key].toString()}</td>
+              </tr>)
+            }
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 };
